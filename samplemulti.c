34aec77c70fb2dc4661cdd5668ff88c6be61ef25
harold-qt <haroldjaytalavera@gmail.com>
2021-10-08 12:01:45 +0800

    unang komit

---
+++ samplemulti.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int a, b;
+
+printf("\ninput first number: ");
+scanf("%d", &a);
+printf("input second number: ");
+scanf("%d", &b);
+
+int res = a*b;
+
+printf("Product is %d", res);
+}
